eb5d7aa1992d82a18c3cfe41e20c5ddc6c74338a
Shivam Kumar Singh <thisisshivam06@gmail.com>
2022-10-14 23:22:49 +0530

    Final Commit

---
+++ data.js
@@ -0,0 +1,11 @@
+const data = [
+  {
+    name: "Shivam Kumar Singh",
+    email: "thisisshivam06@gmail.com",
+    contact: "1111",
+  },
+  { name: "Ramesh", email: "ramesh01@gmail.com" },
+  { name: "Suresh", email: "suresh02@gmail.com" },
+];
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("shivam");
+// const http = require("http");
+// const data = require("./data.js");
+// http
+//   .createServer((req, resp) => {
+//     resp.writeHead(200, { "Content-Type": "application\json" });
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+//   })
+//   .listen(5000);
+
+///// ***** /////
+
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "files");
+
+// for(let i = 0; i < 5; i++){
+// fs.writeFileSync(dirPath+"/hello"+i+".txt","a simple text file");
+// }
+
+// fs.readdir(dirPath, (err, files) => {
+//   files.forEach((item) => {
+//     console.log("file name is ", item);
+//   });
+// });
+
+///// ***** /////
+
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'crud');
+const filePath = `${dirPath}/apple.txt`;
+
+// fs.writeFileSync(filePath, 'This is a Simple text file');
+// fs.readFile(filePath, 'utf-8', (err, item)=>{
+//     console.log(item);
+// })
+
+// fs.appendFile(filePath, 'and file name is apple.txt', (err) => {
+//     if(!err) console.log("file is updated");
+// })
+
+// fs.rename(filePath,`${dirPath}/fruit.txt`, (err) => {
+//     if(!err) console.log("file name is updated")
+// })
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
