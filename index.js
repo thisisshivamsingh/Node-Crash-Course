ead41169880c6ade3d661cc738a5e690abe131f6
Shivam Kumar Singh <thisisshivam06@gmail.com>
2022-10-29 22:20:46 +0530

    Final Commit

--- index.js
+++ index.js
@@ -449,21 +449,21 @@
 
 ///// ***** /////
 
-// const mysql = require("mysql");
+const mysql = require("mysql");
 
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "test",
-// });
-// con.connect((err) => {
-//   if (err) {
-//     console.warn("error");
-//   } else {
-//     console.warn("connected");
-//   }
-// });
-// con.query("select * from users", (err, result) => {
-//   console.warn("result", result);
-// });
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+con.connect((err) => {
+  if (err) {
+    console.warn("error");
+  } else {
+    console.warn("connected");
+  }
+});
+con.query("select * from users", (err, result) => {
+  console.warn("result", result);
+});
